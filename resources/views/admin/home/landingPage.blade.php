f67d1ac1cc7762dd4b017d34bbed31a4bf3e25f5
Fitri Lestari <fitrilestari.github@gmail.com>
2021-11-26 22:23:46 +0700

    Tittle

--- resources/views/admin/home/landingPage.blade.php
+++ resources/views/admin/home/landingPage.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Simessi</title>
+  <title>SIMESSI</title>
   <link rel="shortcut icon" type="image/icon" href="{{ asset ('assets/images/Logo-kabupaten-Tangerang.png') }}">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
